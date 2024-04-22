5acd80d0ba30c4a54d5a122e9a358bc226d70615
JV08 <josephd.vargaso@konradlorenz.edu.co>
2024-04-22 18:38:23 -0500

    Solución al taller
    Se añadió el código necesario para dar solución a lo solicitado

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,37 @@
 package com.mycompany.libropoo;
+import java.time.LocalDateTime;
 
-public class Libro {
-    
+public class Libro { // O1
+private String titulo;
+private String autor;
+private int anioPublicacion;
+private boolean leido;
+
+public Libro (String titulo, String autor, int anioPublicacion){ // O1
+this.titulo = titulo;
+this.autor = autor;
+this.anioPublicacion = anioPublicacion;
+this.leido = false;
+}
+
+public String mostrarInformacion(){ // O1
+
+return ("Título: " + titulo + ", Autor: " + autor + ", Año de Publicación: " + anioPublicacion);
+}
+
+public void marcarLeido (){ // O1
+
+this.leido = true;
+if (leido = true){
+   System.out.println("El libro ha sido leído: Si");
+}
+
+}
+
+public boolean esAntiguo(){ //O1
+LocalDateTime now = LocalDateTime.now();
+int anio = now.getYear();
+return (anio - anioPublicacion) >= 50;
+
+}
 }
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,15 +1,30 @@
 package com.mycompany.libropoo;
 
 import java.util.Scanner;
+import java.time.LocalDateTime;
 
-public class Principal {
-    public static void main(String[] args) {
-        Scanner lect = new Scanner(System.in);
-        String titulo = lect.nextLine();
-        String autor = lect.nextLine();
-        String titulo = lect.nextLine();
-        
+public class Principal { // O1
+public static void main(String[] args) {
+Scanner lect = new Scanner(System.in);
+System.out.println("Ingrese título, autor y año de publicación:");
+String titulo = lect.nextLine();
+String autor = lect.nextLine();
+int anioPublicacion = lect.nextInt();
+LocalDateTime now = LocalDateTime.now();
+int anio = now.getYear();
+if (anioPublicacion > anio){
+    System.out.println("Ingrese un año coherente");
+}else {
+    
+Libro llamada = new Libro(titulo, autor, anioPublicacion);
+llamada.marcarLeido();
+System.out.println(llamada.mostrarInformacion());
+    if(llamada.esAntiguo()){
+        System.out.println("El libro es Antigüo");
     }
-    
-    
 }
+}
+}
+
+
+
