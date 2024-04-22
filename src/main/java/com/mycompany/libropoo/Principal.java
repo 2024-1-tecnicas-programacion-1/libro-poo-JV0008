571f1a03dc2065b55ad6948092524c5702ef6854
X <josephd.vargaso@konradlorenz.edu.co>
2024-04-22 08:22:43 -0500

    Corrección a Solución
    Se añadió el solicitar los datos por consola

--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,15 @@
 package com.mycompany.libropoo;
 
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+        Scanner lect = new Scanner(System.in);
+        String titulo = lect.nextLine();
+        String autor = lect.nextLine();
+        String titulo = lect.nextLine();
+        
+    }
+    
     
 }
